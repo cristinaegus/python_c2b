d9c78d3079f304f64d41da54323a3e4b9a6c0842
Aitor Donado <aitor.donado@gmail.com>
2025-05-19 23:13:55 +0200

    Clase 20-05-25

---
+++ 02a_Strings (Ejercicios).py
@@ -0,0 +1,33 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 24 18:34:31 2023
+
+@author: Aitor Donado
+"""
+
+# Ejercicio 1: Extracción de Subcadenas
+"""
+Dada la cadena saludo1 = "Hola Mundo", extrae la subcadena "Mundo" utilizando indexación.
+"""
+
+
+
+# Ejercicio 2: Formateo de Cadenas
+"""
+Crea una cadena formateada llamada mensaje_personal utilizando el método format. 
+El mensaje debe contener información como el nombre, la edad y la ciudad de residencia.
+"""
+
+
+
+# Ejercicio 3: Reemplazo y Conteo
+"""
+Dada la cadena texto = "Python es un lenguaje de programación. Python es poderoso y fácil de aprender.", realiza 
+las siguientes acciones:
+
+Reemplaza todas las ocurrencias de "Python" por "Java".
+Cuenta cuántas veces aparece la palabra "Python" en la cadena modificada.
+"""
+
+
---
+++ 02b_Listas y otros agregados (Ejercicios).py
@@ -0,0 +1,14 @@
+# 1- Crear una lista (lista1) con los nombres de los meses del año.
+
+
+# 2- Crear un alias (lista2) que sea copia por referencia de la lista.
+
+
+# 3- Clonar la lista1 (lista3).
+
+
+# 4- Añadir a la lista1 “Fin de Año”.
+
+
+# 5- Mostrar la lista2 y la lista3. Veremos que son diferentes
+
